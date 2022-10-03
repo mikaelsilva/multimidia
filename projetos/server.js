6f5e51802d356a106993d6025003bf715f4b681d
mvs22 <mvs2@cin.ufpe.br>
2022-10-02 22:36:12 -0300

    Nova estrutura de salvamento e vercel teste

--- projetos/server.js
+++ projetos/server.js
@@ -22,20 +22,19 @@ app.get('/', (req,res) => {
 })
 
 app.get('/',(req,res) => {
-    var cursor = db.collection('data').find()
+    var cursor = db.collection('multimidia').find()
 })
 
 app.get('/show', (req,res) => {
-    db.collection('data').find().toArray((err,results) => {
+    db.collection('multimidia').find().toArray((err,results) => {
         if (err) return console.log(err)
         //console.log(results)
         res.render('show.ejs', {data: results})
    })
 })
 
-
 app.post('/show', (req,res) => {
-    db.collection('data').insertOne({masterData: req.body}, (err, result) => {
+    db.collection('multimidia').insertOne({"cod":req.body.cod,"lat":req.body.lat,"long":req.body.long}, (err, result) => {
         if (err) return console.log(err)
 
         console.log('Salvamento no banco concluído')
@@ -49,17 +48,18 @@ app.route('/edit/:id').get((req,res) => {
 
     console.log(id)
     console.log(ObjectId(id))
-    db.collection('data').find(ObjectId(id)).toArray((err,result) => {
+    db.collection('multimidia').find(ObjectId(id)).toArray((err,result) => {
         if (err) return res.send(err)
         res.render('edit.ejs', {data: result})
     })
 }).post((req,res) => {
     var id = req.params.id
-    var name = req.body.name
-    var surname = req.body.surname
+    var cod = req.body.cod
+    var lat = req.body.lat
+    var long = req.body.long
     var ObjectId = require('mongodb').ObjectId;
 
-    db.collection('data').updateOne({_id: ObjectId(id)}, {$set: {"masterData":{name: name, surname: surname}}},(err,result) => {
+    db.collection('multimidia').updateOne({_id: ObjectId(id)}, {$set: {"cod": cod, "lat":lat,"long":long}},(err,result) => {
         if (err) return res.send(err)
         res.redirect('/show')
         console.log('Atualizado no Banco de Dados')
@@ -69,7 +69,7 @@ app.route('/edit/:id').get((req,res) => {
 app.route('/delete/:id').get((req,res) => {
     var id = req.params.id
 
-    db.collection('data').deleteOne({'_id': ObjectId(id)}, (err,result) => {
+    db.collection('multimidia').deleteOne({'_id': ObjectId(id)}, (err,result) => {
         if (err) return res.send(500,err)
         console.log('Deletado do Banco de Dados!')
         res.redirect('/show')
